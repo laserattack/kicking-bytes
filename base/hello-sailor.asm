24599159673da20eaf518f5a8ea45e98a8bbe7f5
serr <sc7227484@gmail.com>
2025-08-25 17:47:34 +0300

    quick update

---
+++ base/hello-sailor.asm
@@ -0,0 +1,31 @@
+; vim: syntax=fasm:
+
+format ELF64 executable
+
+macro write fd, buf, count
+{
+    mov rax, 1
+    mov rdi, fd
+    mov rsi, buf
+    mov rdx, count
+    syscall
+}
+
+macro exit status 
+{
+   mov rax, 60
+   mov rdi, status
+   syscall
+}
+
+segment readable executable
+entry main
+main:
+    repeat 10
+        write 1, hello_sailor_msg, hello_sailor_msg_len
+    end repeat
+    exit 0
+
+segment readable writeable
+hello_sailor_msg db "Hello, Sailor!", 10
+hello_sailor_msg_len = $ - hello_sailor_msg 
